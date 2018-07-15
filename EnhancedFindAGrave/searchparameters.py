76a1ca3be334342982e69f9892fd4ebc6a2fdd9e
Jamie22 <jamie2244@gmail.com>
2018-07-14 21:19:06 -0400

    Update for new Find A Grave

--- EnhancedFindAGrave/searchparameters.py
+++ EnhancedFindAGrave/searchparameters.py
@@ -3,6 +3,9 @@ first_name = "Jane"
 middle_name = ""
 last_name = "Smith"
 
+# Include nickname
+include_nickname = False
+
 # Include maiden name(s) in my search
 include_maiden = False
 
@@ -14,23 +17,22 @@ max_birth_year = 1830
 min_death_year = 1900
 max_death_year = 1930
 
-# 0 = All Countries
-# 4 = The United States
-# 5 = England
-# 10 = Canada
-# See country drop down HTML code on Find A Grave search page for more country IDs
-country_id = 0
-
-# 0 = All States, or if searching country other than the Unites States
-# See state drop down HTML code on Find A Grave search page for state IDs
-state_id = 0
+# Accepted values, with numeric ID after the underscore:
+# Empty string for all locations
+# country_
+# state_ (for state, province, county, etc.)
+# city_
+# Countries include:
+# country_4 = The United States
+# country_5 = England
+# country_10 = Canada
+location_id = ''
 
 # all = All Names
-# 1 = Added in last 7 days
+# 1 = Added in last 24 hours
+# 7 = Added in last 7 days
 # 30 = Added in last 30 days
 # 90 = Added in last 90 days
-# np = Names with no grave photo
-# p = Names with grave photo
 date_filter = 'all'
 
 # n = Name
@@ -40,3 +42,17 @@ date_filter = 'all'
 # Note that order is affected by which pages load first, so results won't be in the same order as they would be on
 # Find A Grave
 order = 'n'
+
+# Accepted values:
+# Empty string for no filter
+# sponsored
+# famous
+memorial_type = ''
+
+# Accepted values:
+# Empty string for no filter
+# photos
+# nophotos
+photo_filter = ''
+
+has_flowers = False
